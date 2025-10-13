ca6b7891cdff3a0555b2f993e800f7ef4f90aa2d
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-10-13 09:43:36 +0200

    add more textobject functionality in mini.ai

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -1,26 +1,30 @@
 vim.pack.add({
-	{ src = "https://github.com/nvim-mini/mini.ai" },
+    { src = "https://github.com/nvim-mini/mini.ai" },
 })
 
+local ai = require("mini.ai")
 require("mini.ai").setup({
     custom_textobjects = {
-      -- -- Tweak argument textobject
-      -- a = require('mini.ai').gen_spec.argument({ brackets = { '%b()' } }),
-      --
-      -- -- Disable brackets alias in favor of builtin block textobject
-      -- b = false,
-      --
-      -- -- Now `vax` should select `xxx` and `vix` - middle `x`
-      -- x = { 'x()x()x' },
-
-      -- Whole buffer
-      g = function()
-        local from = { line = 1, col = 1 }
-        local to = {
-          line = vim.fn.line('$'),
-          col = math.max(vim.fn.getline('$'):len(), 1)
-        }
-        return { from = from, to = to }
-      end
+        o = ai.gen_spec.treesitter({ -- code block
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }),
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+        d = { "%f[%d]%d+" }, -- digits
+        e = { -- Word with case
+            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+            "^().*()$",
+        },
+        -- Whole buffer
+        g = function()
+            local from = { line = 1, col = 1 }
+            local to = {
+                line = vim.fn.line('$'),
+                col = math.max(vim.fn.getline('$'):len(), 1)
+            }
+            return { from = from, to = to }
+        end
     }
 })
