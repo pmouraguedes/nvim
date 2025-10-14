c82d6f474cad71f37a623d1ea873df39cd55bdcd
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-10-14 11:19:33 +0200

    add auto search hl native plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,6 +5,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/nvim-mini/mini.align" },
 })
+vim.cmd("packadd nohlsearch")
 
 require("mason").setup()
 require("mini.icons").setup()
