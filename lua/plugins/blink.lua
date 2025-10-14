c239205013ef326e0c38f884f018a6e3b68436ac
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-10-14 12:48:05 +0200

    add blink and git plugins

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,17 @@
+vim.pack.add({
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
+    { src = "https://github.com/rafamadriz/friendly-snippets" },
+    { src = "https://github.com/saghen/blink.cmp" },
+})
+
+require("blink.cmp").setup({
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+            sql = { 'snippets', 'dadbod', 'buffer' },
+        },
+        providers = {
+            dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+    },
+})
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,6 +4,8 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/nvim-mini/mini.align" },
+	{ src = "https://github.com/sindrets/diffview.nvim" },
+	{ src = "https://github.com/NeogitOrg/neogit" },
 })
 vim.cmd("packadd nohlsearch")
 
@@ -11,3 +13,5 @@ require("mason").setup()
 require("mini.icons").setup()
 require("plenary").setup()
 require("mini.align").setup()
+require("diffview").setup()
+require("neogit").setup()
