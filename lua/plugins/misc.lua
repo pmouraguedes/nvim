b434a40d77cb1dfd6ef18f6853ceca6cc028c444
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-10-16 14:49:38 +0200

    removee plenary setup call

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,17 +1,16 @@
 vim.pack.add({
-	{ src = "https://github.com/nvim-mini/mini.icons" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/nvim-mini/mini.align" },
-	{ src = "https://github.com/sindrets/diffview.nvim" },
-	{ src = "https://github.com/NeogitOrg/neogit" },
+    { src = "https://github.com/nvim-mini/mini.icons" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-mini/mini.align" },
+    { src = "https://github.com/sindrets/diffview.nvim" },
+    { src = "https://github.com/NeogitOrg/neogit" },
 })
 vim.cmd("packadd nohlsearch")
 
 require("mason").setup()
 require("mini.icons").setup()
-require("plenary").setup()
 require("mini.align").setup()
 require("diffview").setup()
 require("neogit").setup()
