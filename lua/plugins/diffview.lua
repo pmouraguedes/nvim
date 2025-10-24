d283f4684ee4d343f3ac36b71b55de37cd3ed6bb
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-10-24 12:00:20 +0200

    improve diffview layout

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,11 +1,19 @@
 vim.pack.add({
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
     { src = "https://github.com/sindrets/diffview.nvim" },
 })
 
-require("diffview").setup()
+require'nvim-web-devicons'.setup {}
+
+require("diffview").setup({
+    file_panel = {
+        win_config = { -- See |diffview-config-win_config|
+            position = "bottom",
+            height = 14,
+        },
+    },
+})
 
 local map = vim.keymap.set
-
 map("n", "<leader>go", "<cmd>DiffviewOpen<cr>", { desc = "Open diff view" })
 map("n", "<leader>gc", "<cmd>DiffviewClose<cr>", { desc = "Open diff view" })
-
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,14 +1,12 @@
 vim.pack.add({
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/sindrets/diffview.nvim" },
     { src = "https://github.com/NeogitOrg/neogit" },
     { src = "https://github.com/dimtion/guttermarks.nvim" },
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
 vim.cmd("packadd nohlsearch")
 
-require("diffview").setup()
 require("neogit").setup()
 require("guttermarks").setup()
 require('gitsigns').setup()
